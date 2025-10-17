255c7c3775e66db3a93ba0951764002bb10babf3
UnwieldyLizard <unwieldylizard@gmail.com>
2025-10-16 19:36:35 -0700

    finished eddington inverter, untested

--- src/eddington_inverter.rs
+++ src/eddington_inverter.rs
@@ -3,22 +3,25 @@ use std::f64::consts::PI;
 use crate::forces::GG;
 
 const SPACIAL_GRID_NUM: usize = 1000;
+const VELOCITY_GRID_NUM: usize = 1000;
 const ENERGY_GRID_NUM: usize = 1000;
 const LOG_OFFSET: f64 = 1e-10;
 
-fn compute_phase_space_density(rho: fn(f64) -> f64, r_cutoff: &f64, energy_cutoff: &f64, mass: &f64, cuspy: bool) {
+fn compute_phase_space_density(rho: fn(f64) -> f64, r_cutoff: &f64, v_cutoff: &f64, mass: &f64, cuspy: bool) -> anyhow::Result<[[f64; VELOCITY_GRID_NUM]; SPACIAL_GRID_NUM]> {
     let mut r_points: [f64; SPACIAL_GRID_NUM] = [0.0; SPACIAL_GRID_NUM];
     for i in 0..SPACIAL_GRID_NUM {
-        r_points[i] = (i as f64) * r_cutoff / (SPACIAL_GRID_NUM as f64);
+        r_points[i] = (i as f64) * r_cutoff / ((SPACIAL_GRID_NUM - 1) as f64);
     }
 
     let rho_points = generate_rho_points(rho, &r_points);
     let potential_points = generate_potential_points(&rho_points, &r_points);
     let drho_dV = generate_drho_dV(&rho_points, &potential_points, cuspy);
 
+    let energy_cutoff: f64 = 0.5 * mass * v_cutoff * v_cutoff + potential_points[SPACIAL_GRID_NUM - 1]; //max potential occurs at largest radius
+
     let mut energy_points: [f64; ENERGY_GRID_NUM] = [0.0; ENERGY_GRID_NUM];
     for j in 0..ENERGY_GRID_NUM {
-        energy_points[j] = (j as f64) * energy_cutoff / (ENERGY_GRID_NUM as f64);
+        energy_points[j] = potential_points[0] + (j as f64) * (energy_cutoff - potential_points[0]) / ((ENERGY_GRID_NUM - 1) as f64); //min energy is min potential at r=0
     }
     let mut integral: [f64; ENERGY_GRID_NUM] = [0.0; ENERGY_GRID_NUM];
 
@@ -35,12 +38,37 @@ fn compute_phase_space_density(rho: fn(f64) -> f64, r_cutoff: &f64, energy_cutof
         }
     }
 
-    let mut f: [f64; ENERGY_GRID_NUM] = [0.0; ENERGY_GRID_NUM];
+    let mut f_of_energy: [f64; ENERGY_GRID_NUM] = [0.0; ENERGY_GRID_NUM];
 
     for j in 0..(ENERGY_GRID_NUM - 1) {
-        f[j] = (integral[j+1] - integral[j]) / (energy_points[j+1] - energy_points[j]);
-        f[j] /= 8.0_f64.sqrt() * PI * PI;
+        f_of_energy[j] = (integral[j+1] - integral[j]) / (energy_points[j+1] - energy_points[j]);
+        f_of_energy[j] /= 8.0_f64.sqrt() * PI * PI;
+        if f_of_energy[j] < 0.0 {
+            panic!("Negative distribution function, Eddington Inversion has failed");
+        }
     }
+    
+    let mut v_points: [f64; VELOCITY_GRID_NUM] = [0.0; VELOCITY_GRID_NUM];
+    for j in 0..VELOCITY_GRID_NUM {
+        v_points[j] = (j as f64) * v_cutoff / ((VELOCITY_GRID_NUM - 1) as f64);
+    }
+
+    let mut f: [[f64; ENERGY_GRID_NUM]; SPACIAL_GRID_NUM] = [[0.0; ENERGY_GRID_NUM]; SPACIAL_GRID_NUM];
+
+    for i in 0..SPACIAL_GRID_NUM {
+        for j in 0..VELOCITY_GRID_NUM {
+            let energy: f64 = 0.5 * mass * v_points[j] * v_points[j] + potential_points[i];
+            let mut e_arg: usize = 0;
+            for k in 0..ENERGY_GRID_NUM {
+                if (energy_points[k] - energy).abs() < (energy_points[e_arg] - energy).abs() {
+                    e_arg = k;
+                }
+            }
+            f[i][j] = f_of_energy[e_arg];
+        }
+    }
+
+    Ok(f)
 }
 
 fn generate_rho_points(rho: fn(f64) -> f64, r_points: &[f64; SPACIAL_GRID_NUM]) -> [f64; SPACIAL_GRID_NUM] {
